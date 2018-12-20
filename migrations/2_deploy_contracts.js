1014a1384046c25e6f1bec8ddad4599dc0f924f2
Saravana Perumal Ramachandran <speaktosp@gmail.com>
2018-12-20 11:33:23 -0500

    Update Contracts 1

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
-var CampaignFactory = artifacts.require("./CampaignFactory.sol");
+//var CampaignFactory = artifacts.require("./CampaignFactory.sol");
+
+var Lottery = artifacts.require("./Lottery.sol");
 
 module.exports = function(deployer) {
-   deployer.deploy(CampaignFactory);
+   //deployer.deploy(CampaignFactory);
+   deployer.deploy(Lottery);
+   
 };
